f89f577f395add829e198769af6ec367a931b052
tejasgupta46 <tejasguptacse@gmail.com>
2026-08-17 23:51:01 +0530

    few change

--- project2/script.js
+++ project2/script.js
@@ -10,15 +10,16 @@ const update = () =>{
       seconds.style.transform = `rotate(${secdegrees}deg)`
 
       const mins = now.getMinutes()
-      const mindegrees =  ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+      const mindegrees =  ((mins / 60) * 360) + ((sec/60)*6) + 90;
       minutes.style.transform = `rotate(${mindegrees}deg)`;
       
       const hr = now.getHours()
-       const hrdegrees = ((hr / 12) * 360) + ((mins/60)*30) + 90;
+       const hrdegrees = ((hr / 12) * 360) + ((mins/60)*26) + 90;
        hours.style.transform = `rotate(${hrdegrees}deg)`;
 
        
        console.log(sec)
+       console.log(hr)
     //    console.log(min)
     //    console.log(hr)
 } 
